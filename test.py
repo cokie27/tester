315b6d15120db1f99e0cd8730b0583f38d981bab
cokie27 <tukangsampah27@gmail.com>
2019-03-13 23:19:37 +0700

    ok

---
+++ test.py
@@ -0,0 +1,26 @@
+import time
+import os
+os.system('clear')
+print 
+print ("mastah ajarin")
+print 
+time.sleep(1)
+print ("jangan pelit ")
+print 
+time.sleep(1)
+print ("nanti masuk neraka")
+print 
+time.sleep(1)
+print ("test script python")
+print 
+time.sleep(1)
+print ("jangan di bully")
+print 
+
+ngerti = raw_input("lu ngerti kaga stah [ya/belum]: ")
+
+if  ngerti == "ya":
+ print ("good pinter mastah que")
+
+if  ngerti == "belum" :
+ print ("auto penghuni neraka lu stah")
